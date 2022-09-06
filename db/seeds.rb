941369122a32b45727da5278627eb463d61eb4d5
arthuroliver <arthur1606@gmail.com>
2022-09-06 12:02:17 -0300

    display posts on home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,42 +9,42 @@ def seed_users
     )  
     user_id = user_id + 1
   end
+end
 
-  def seed_categories
-    hobby = ['Arts', 'Crafts', 'Sports', 'Sciences', 'Collecting', 'Reading', 'Other']
-    study = ['Arts and Humanities', 'Physical Science and Engineering', 'Math and Logic',
-            'Computer Science', 'Data Science', 'Economics and Finance', 'Business',
-            'Social Sciences', 'Language', 'Other']
-    team = ['Study', 'Development', 'Arts and Hobby', 'Other']
+def seed_categories
+  hobby = ['Arts', 'Crafts', 'Sports', 'Sciences', 'Collecting', 'Reading', 'Other']
+  study = ['Arts and Humanities', 'Physical Science and Engineering', 'Math and Logic',
+          'Computer Science', 'Data Science', 'Economics and Finance', 'Business',
+          'Social Sciences', 'Language', 'Other']
+  team = ['Study', 'Development', 'Arts and Hobby', 'Other']
 
-    hobby.each do |name|
-      Category.create(branch: 'hobby', name: name)
-    end
+  hobby.each do |name|
+    Category.create(branch: 'hobby', name: name)
+  end
 
-    study.each do |name|
-      Category.create(branch: 'study', name: name)
-    end
+  study.each do |name|
+    Category.create(branch: 'study', name: name)
+  end
 
-    team.each do |name|
-      Category.create(branch: 'team', name: name)
-    end
-
-    def seed_posts
-      categories = Category.all
-      categories.each do |category|
-        5.times do
-          Post.create(
-            title: Faker::Lorem.sentences[0],
-            content: Faker::Lorem.sentences[0],
-            user_id: rand(1..9),
-            category_id: category.id,
-          )
-        end
-      end
-    end
-
-    seed_users
-    seed_categories
-    seed_posts
+  team.each do |name|
+    Category.create(branch: 'team', name: name)
   end
 end
+
+def seed_posts
+  categories = Category.all
+  categories.each do |category|
+    5.times do
+      Post.create(
+        title: Faker::Lorem.sentences[0],
+        content: Faker::Lorem.sentences[0],
+        user_id: rand(1..9),
+        category_id: category.id,
+      )
+    end
+  end
+end
+
+seed_users
+seed_categories
+seed_posts
